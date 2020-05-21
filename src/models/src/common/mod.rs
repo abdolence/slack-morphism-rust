586fbd9a7031527bdae352574cfe62bb0b002cbe
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2020-05-21 19:48:42 +0300

    Slack push event models update

--- src/models/src/common/mod.rs
+++ src/models/src/common/mod.rs
@@ -1,7 +1,7 @@
 use rvs_derive::ValueStruct;
 use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc, TimeZone};
-use chrono::serde::ts_seconds;
+use chrono::serde::ts_milliseconds;
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
 pub struct SlackTs(pub String);
@@ -13,6 +13,12 @@ impl SlackTs {
         Ok(Utc.timestamp_millis(ts_int))
     }
 }
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
+pub struct SlackTeamId(pub String);
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
+pub struct SlackAppId(pub String);
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
 pub struct SlackChannelId(pub String);
@@ -27,4 +33,10 @@ pub struct SlackConversationId(pub String);
 pub struct SlackActionId(pub String);
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
-pub struct SlackDateTime(#[serde(with = "ts_seconds")] pub DateTime<Utc>);
+pub struct SlackEventId(pub String);
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
+pub struct SlackUserId(pub String);
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, ValueStruct)]
+pub struct SlackDateTime(#[serde(with = "ts_milliseconds")] pub DateTime<Utc>);
--- src/models/src/events/mod.rs
+++ src/models/src/events/mod.rs
@@ -9,19 +9,39 @@ use crate::common::*;
 pub enum SlackPushEvent {
     #[serde(rename = "url_verification")]
     UrlVerification(SlackUrlVerificationEvent),
+    #[serde(rename = "event_callback")]
+    EventCallback(SlackEventCallback),
+    #[serde(rename = "app_rate_limited")]
     AppRateLimited(SlackAppRateLimitedEvent)
 }
 
 #[skip_serializing_none]
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
 pub struct SlackUrlVerificationEvent {
-    challenge: String
+    pub challenge: String
 }
 
 #[skip_serializing_none]
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
 pub struct SlackAppRateLimitedEvent {
-    team_id: String,
-    minute_rate_limited: SlackDateTime,
-    api_app_id: String
+    pub team_id: String,
+    pub minute_rate_limited: SlackDateTime,
+    pub api_app_id: String
+}
+
+#[skip_serializing_none]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
+pub struct  SlackEventCallback {
+    pub team_id: SlackTeamId,
+    pub api_app_id: SlackAppId,
+    pub event: SlackEventCallbackBody,
+    pub event_id: SlackEventId,
+    pub event_time: SlackDateTime,
+    pub authed_users: Option<Vec<SlackUserId>>
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum SlackEventCallbackBody {
+
 }
