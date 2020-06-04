145602440bfa3d99cc38b6dac786cfc06203c1e8
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2020-06-04 18:07:15 +0300

    Scrolling items support functions

--- src/client/src/api/users.rs
+++ src/client/src/api/users.rs
@@ -68,7 +68,7 @@ impl SlackApiScrollableResponse for SlackApiUsersListResponse {
             .flatten()
     }
 
-    fn scrollable_items<'a>(&'a self) -> Box<dyn Iterator<Item = Self::ResponseItemType> +'a> {
+    fn scrollable_items<'a>(&'a self) -> Box<dyn Iterator<Item = Self::ResponseItemType> + 'a> {
         Box::new(self.members.clone().into_iter())
     }
 }
