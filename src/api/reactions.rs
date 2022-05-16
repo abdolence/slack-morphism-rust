9674d05dbccdbe8443d66b5f4e66031ef336d544
Kyosuke Kawakami <k_kawakami@yumemi.co.jp>
2022-05-16 20:47:33 +0900

    Support reactions.get request
    (cherry picked from commit dc1883afb85a6881f0edfdcfd4a6bd8256353526)

---
+++ src/api/reactions.rs
@@ -0,0 +1,56 @@
+//!
+//! Support for Slack Reactions API methods
+//!
+
+use rsb_derive::Builder;
+use rvstruct::ValueStruct;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
+use crate::ratectl::SLACK_TIER3_METHOD_CONFIG;
+use crate::SlackClientSession;
+use crate::{ClientResult, SlackClientHttpConnector};
+use slack_morphism_models::*;
+
+impl<'a, SCHC> SlackClientSession<'a, SCHC>
+where
+    SCHC: SlackClientHttpConnector + Send,
+{
+    ///
+    /// https://api.slack.com/methods/reactions.get
+    ///
+    pub async fn reactions_get(
+        &self,
+        req: &SlackApiReactionsGetRequest,
+    ) -> ClientResult<SlackApiReactionsGetResponse> {
+        self.http_session_api
+            .http_get(
+                "reactions.get",
+                &vec![
+                    ("channel", req.channel.as_ref().map(|x| x.value())),
+                    ("file", req.file.as_ref().map(|x| x.value())),
+                    ("full", req.full.map(|v| v.to_string()).as_ref()),
+                    ("timestamp", req.timestamp.as_ref().map(|x| x.value())),
+                ],
+                Some(&SLACK_TIER3_METHOD_CONFIG),
+            )
+            .await
+    }
+}
+
+#[skip_serializing_none]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
+pub struct SlackApiReactionsGetRequest {
+    pub channel: Option<SlackChannelId>,
+    pub file: Option<SlackFileId>,
+    pub full: Option<bool>,
+    pub timestamp: Option<SlackTs>,
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+#[allow(clippy::large_enum_variant)]
+pub enum SlackApiReactionsGetResponse {
+    Message(SlackMessage),
+    File(SlackFile),
+}
