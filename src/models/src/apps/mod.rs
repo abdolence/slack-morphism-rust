f6d364f8e7a09189f5519d8c9361776bc8ae0d45
Naoki Ikeguchi (YUMEMI Inc.) <n_ikeguchi@yumemi.co.jp>
2022-07-02 12:25:52 +0900

    fix: Add missing imports

--- src/models/src/apps/mod.rs
+++ src/models/src/apps/mod.rs
@@ -2,6 +2,10 @@ pub mod manifest;
 
 pub use manifest::*;
 
+use rsb_derive::Builder;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
+
 use crate::{SlackClientId, SlackClientSecret, SlackSigningSecret, SlackVerificationToken};
 
 #[skip_serializing_none]
