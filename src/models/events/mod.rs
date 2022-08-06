29d923e91933809d3ec089db34420c0c8a106930
Sehyun Park <doodot.dev@gmail.com>
2022-08-06 22:02:40 +0900

    feat: Add SlackViewSubmissionResponse (#143)

--- src/models/events/mod.rs
+++ src/models/events/mod.rs
@@ -5,11 +5,13 @@ mod authorization;
 mod command;
 mod interaction;
 mod push;
+mod view;
 
 pub use authorization::*;
 pub use command::*;
 pub use interaction::*;
 pub use push::*;
+pub use view::*;
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Serialize, Deserialize, ValueStruct)]
 pub struct SlackEventId(pub String);
