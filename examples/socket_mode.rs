f1dc1ab8142b03f4b10eb0b840717c8a6c250941
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-02-11 11:41:16 +0100

    Socket Mode example trace to debug

--- examples/socket_mode.rs
+++ examples/socket_mode.rs
@@ -111,7 +111,7 @@ pub fn config_env_var(name: &str) -> Result<String, String> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let subscriber = tracing_subscriber::fmt()
-        .with_env_filter("slack_morphism=trace")
+        .with_env_filter("slack_morphism=debug")
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
