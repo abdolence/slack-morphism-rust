18ec1fc384d9b8dd6700ee692046bfdcc698ed36
Abdulla Abdurakhmanov <me@abdolence.dev>
2023-03-12 13:19:35 +0100

    Small rename

--- src/hyper_tokio/connector.rs
+++ src/hyper_tokio/connector.rs
@@ -406,8 +406,8 @@ impl<H: 'static + Send + Sync + Clone + connect::Connect> SlackClientHttpConnect
                         )
                         .header("content-type", "application/x-www-form-urlencoded");
 
-                        let toke_body_prefix = context_token.map_or_else(String::new, |t| {
-                            format!("token={}&", t.token_value.value())
+                        let toke_body_prefix = context_token.map_or_else(String::new, |token| {
+                            format!("token={}&", token.token_value.value())
                         });
                         let full_body = toke_body_prefix + &post_url_form;
                         http_request.body(full_body.into()).map_err(|e| e.into())
