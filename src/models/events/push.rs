ca88bdd3e610922815e4b8fb5d83697277be6e88
Natsuki Ikeguchi <me@s6n.jp>
2023-12-14 22:22:35 +0900

    fix!: Include SlackMessageSender in SlackMessageEventEdited (#230)
    * fix!: Include SlackMessageSender in SlackMessageEventEdited
    
    BREAKING CHANGE: SlackMessageEventEdited::user is moved to sender.user and now it is optional.
    
    Signed-off-by: Natsuki Ikeguchi <n_ikeguchi@yumemi.co.jp>
    
    * test: Assert value of sender.user and sender.bot_id
    
    Signed-off-by: Natsuki Ikeguchi <n_ikeguchi@yumemi.co.jp>
    
    ---------
    
    Signed-off-by: Natsuki Ikeguchi <n_ikeguchi@yumemi.co.jp>

--- src/models/events/push.rs
+++ src/models/events/push.rs
@@ -185,7 +185,8 @@ pub struct SlackAppMentionEvent {
 pub struct SlackMessageEventEdited {
     #[serde(flatten)]
     pub content: Option<SlackMessageContent>,
-    pub user: SlackUserId,
+    #[serde(flatten)]
+    pub sender: SlackMessageSender,
     pub ts: SlackTs,
     pub edited: SlackMessageEdited,
 }
@@ -385,6 +386,34 @@ mod test {
             }) => {
                 assert_eq!(subtype, Some(SlackMessageEventType::MessageChanged));
                 if let Some(message) = message {
+                    assert_eq!(message.sender.user, Some("UXXXXXXXXXX".into()));
+                    assert_eq!(message.sender.bot_id, None);
+                    assert_eq!(message.ts, "1701735043.989889".into());
+                    assert_eq!(message.edited.ts, "1701743154.000000".into());
+                    assert_eq!(
+                        message.content.unwrap().text,
+                        Some("edited message".to_string())
+                    );
+                } else {
+                    panic!("Message is None");
+                }
+            }
+            _ => panic!("Unexpected event type"),
+        }
+    }
+
+    #[test]
+    fn test_slack_event_message_changed_by_bot_event() {
+        let payload = include_str!("./fixtures/message_changed_by_bot.json");
+        let event: SlackPushEventCallback = serde_json::from_str(payload).unwrap();
+        match event.event {
+            SlackEventCallbackBody::Message(SlackMessageEvent {
+                subtype, message, ..
+            }) => {
+                assert_eq!(subtype, Some(SlackMessageEventType::MessageChanged));
+                if let Some(message) = message {
+                    assert_eq!(message.sender.user, None);
+                    assert_eq!(message.sender.bot_id, Some("BXXXXXXXXXX".into()));
                     assert_eq!(message.ts, "1701735043.989889".into());
                     assert_eq!(message.edited.ts, "1701743154.000000".into());
                     assert_eq!(
